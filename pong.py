5efb1d8130a9142d16efd1acbb217a5e336d8897
chris-cozy <csande9@clemson.edu>
2023-02-23 11:34:30 -0500

    updated main game file to include ball and enemy

--- pong.py
+++ pong.py
@@ -1,8 +1,12 @@
 import pygame
 import sys
 from paddle import Paddle
+from ball import Ball
 
 pygame.init()
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
 screenColor = (255, 204, 203)
@@ -13,16 +17,30 @@ surface = pygame.display.set_mode(SCREEN_SIZE)
 # Player Paddle
 rectColor = (119, 198, 110)
 rectWidth, rectHeight = 10, 100
-rectX, rectY = 0, (SCREEN_HEIGHT/2)
-rectSpeed = 1
+rectX, rectY = 0, ((SCREEN_HEIGHT/2) - (rectHeight/2))
 
 playerPaddle = Paddle(rectColor, rectWidth, rectHeight)
 playerPaddle.rect.x = rectX
 playerPaddle.rect.y = rectY
 
+# Enemy Paddle
+
+enemyPaddle = Paddle(rectColor, rectWidth, rectHeight)
+enemyPaddle.rect.x = SCREEN_WIDTH - rectWidth
+enemyPaddle.rect.y = rectY
+
+# Ball
+ballWidth, ballHeight = 15, 15
+
+ball = Ball(rectColor, ballWidth, ballHeight)
+ball.rect.x = SCREEN_WIDTH/2
+ball.rect.y = SCREEN_HEIGHT/2
+
 
 spriteList = pygame.sprite.Group()
 spriteList.add(playerPaddle)
+spriteList.add(enemyPaddle)
+spriteList.add(ball)
 
 playing = True
 
@@ -40,13 +58,23 @@ while playing:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
         if (playerPaddle.rect.y > 0):
-            playerPaddle.move_up(5)
+            playerPaddle.move_up(playerPaddle.velocity)
 
     if keys[pygame.K_DOWN]:
         if (playerPaddle.rect.y + rectHeight) < SCREEN_HEIGHT:
-            playerPaddle.move_down(5)
+            playerPaddle.move_down(playerPaddle.velocity)
 
     spriteList.update()
+
+    # bouncing logic
+    if ball.rect.x >= (SCREEN_WIDTH - ballWidth):
+        ball.velocity[0] = -ball.velocity[0]
+    if ball.rect.x <= 0:
+        ball.velocity[0] = -ball.velocity[0]
+    if ball.rect.y >= (SCREEN_HEIGHT - ballHeight):
+        ball.velocity[1] = -ball.velocity[1]
+    if ball.rect.y <= 0:
+        ball.velocity[1] = -ball.velocity[1]
 
     surface.fill(screenColor)
 
