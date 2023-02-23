d9b9228d2ba196a214a17c36231d1ff73b275a7b
chris-cozy <csande9@clemson.edu>
2023-02-23 14:12:38 -0500

    implemented enemy ai

--- pong.py
+++ pong.py
@@ -8,38 +8,38 @@ pygame.init()
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-colors = [
+spriteColors = [
     (102, 84, 94),
     (163, 145, 147),
     (170, 111, 115),
     (238, 169, 144)
 ]
+screenColor = (246, 224, 181)
+playerColor = spriteColors[randint(0, 3)]
+enemyColor = spriteColors[randint(0, 3)]
+ballColor = spriteColors[randint(0, 3)]
 
 SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
-screenColor = (246, 224, 181)
 pygame.display.set_caption("Cozy Pong")
 surface = pygame.display.set_mode(SCREEN_SIZE)
 
 
-# Player Paddle
-playerColor = colors[randint(0, 3)]
-enemyColor = colors[randint(0, 3)]
-ballColor = colors[randint(0, 3)]
 rectWidth, rectHeight = 10, 100
+ballWidth, ballHeight = 15, 15
 rectX, rectY = 0, ((SCREEN_HEIGHT/2) - (rectHeight/2))
 
+# Player Paddle
 playerPaddle = Paddle(playerColor, rectWidth, rectHeight)
 playerPaddle.rect.x = rectX
 playerPaddle.rect.y = rectY
 
 # Enemy Paddle
-
 enemyPaddle = Paddle(enemyColor, rectWidth, rectHeight)
 enemyPaddle.rect.x = SCREEN_WIDTH - rectWidth
 enemyPaddle.rect.y = rectY
 
 # Ball
-ballWidth, ballHeight = 15, 15
+
 
 ball = Ball(ballColor, ballWidth, ballHeight)
 ball.rect.x = SCREEN_WIDTH/2
@@ -93,6 +93,15 @@ while playing:
         ball.velocity[1] = -ball.velocity[1]
     if ball.rect.y <= 0:
         ball.velocity[1] = -ball.velocity[1]
+
+    # enemy ai
+    if ball.velocity[0] > 0:
+        if ball.velocity[1] > 0:
+            if (enemyPaddle.rect.y + rectHeight) < SCREEN_HEIGHT:
+                enemyPaddle.move_down(enemyPaddle.velocity)
+        else:
+            if (enemyPaddle.rect.y > 0):
+                enemyPaddle.move_up(enemyPaddle.velocity)
 
     # Collisions
     if pygame.sprite.collide_mask(ball, playerPaddle) or pygame.sprite.collide_mask(ball, enemyPaddle):
