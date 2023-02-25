32039b3c533b646943831c992f79b241ccabe108
chris-cozy <csande9@clemson.edu>
2023-02-25 17:23:25 -0500

    organized paddle logic

--- paddle.py
+++ paddle.py
@@ -18,8 +18,22 @@ class Paddle(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect()
 
+    def set_pos(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
+
     def move_up(self, pixels):
         self.rect.y -= pixels
 
     def move_down(self, pixels):
         self.rect.y += pixels
+
+    def check_keys(self, pHeight, screenHeight):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            if (self.rect.y > 0):
+                self.move_up(self.velocity)
+
+        if keys[pygame.K_DOWN]:
+            if (self.rect.y + pHeight) < screenHeight:
+                self.move_down(self.velocity)
