c77e1d3552227bcc238ed9e870168806c4d0011b
chris-cozy <csande9@clemson.edu>
2023-08-09 10:16:13 -0400

    Added dpcumentation and updated naming conventions

--- pong_game/screen.py
+++ pong_game/screen.py
@@ -1,32 +1,53 @@
+"""
+This module contains the Screen class, representing the screen object in the Pong game.
+"""
 import pygame
 
 pygame.init()
 
 
 class Screen():
+    """
+    Represents the screen object.
+    """
 
-    def __init__(self, caption, screenSize, screenWidth, screenHeight, screenColor):
+    def __init__(self, caption, screen_size, screen_width, screen_height, screen_color):
+        """
+        Initialize the screen with size, width, height, color, and a caption.
+        """
         super().__init__()
 
         pygame.display.set_caption(caption)
 
-        self.surface = pygame.display.set_mode(screenSize)
-        self.screenWidth = screenWidth
-        self.screenHeight = screenHeight
-        self.screenColor = screenColor
+        self.surface = pygame.display.set_mode(screen_size)
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.screen_color = screen_color
 
     def fill(self):
-        self.surface.fill(self.screenColor)
+        """
+        Fill the screen with the color.
+        """
+        self.surface.fill(self.screen_color)
 
-    def draw(self, spriteList):
-        spriteList.draw(self.surface)
+    def draw(self, sprite_list):
+        """
+        Draw the list of sprites on the screen.
+        """
+        sprite_list.draw(self.surface)
 
-    def display_text(self, playerScore, enemyScore, playerColor, enemyColor):
+    def display_text(self, player_score, enemy_score, player_color, enemy_color):
+        """
+        Display text on the screen. Player's score, Enemy's score, Player color, Enemy color.
+        """
         font = pygame.font.Font(None, 74)
-        text = font.render(str(playerScore), 1, playerColor)
-        self.surface.blit(text, (self.screenWidth/4, 10))
-        text = font.render(str(enemyScore), 1, enemyColor)
-        self.surface.blit(text, ((self.screenWidth/4) * 3, 10))
+        text = font.render(str(player_score), 1, player_color)
+        self.surface.blit(text, (self.screen_width/4, 10))
+        text = font.render(str(enemy_score), 1, enemy_color)
+        self.surface.blit(text, ((self.screen_width/4) * 3, 10))
 
     def flip(self):
+        """
+        Flip the screen.
+        """
         pygame.display.flip()
--- pong_game/sprites/paddle.py
+++ pong_game/sprites/paddle.py
@@ -1,11 +1,25 @@
 import pygame
 
+"""
+This module contains the Paddle class, representing the paddle objects in the Pong game.
+"""
 BLACK = (0, 0, 0)
 
 
 class Paddle(pygame.sprite.Sprite):
+    """
+    Represents the paddles in the Pong game.
+
+    Attributes:
+        color (tuple): RGB color tuple for the paddle.
+        width (int): Width of the paddle.
+        height (int): Height of the paddle.
+    """
 
     def __init__(self, color, width, height):
+        """
+        Initialize the paddle with given color, width, and height.
+        """
         super().__init__()
 
         self.image = pygame.Surface([width, height])
@@ -19,21 +33,33 @@ class Paddle(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
     def set_pos(self, x, y):
+        """
+        Set the position of the paddle based on coords.
+        """
         self.rect.x = x
         self.rect.y = y
 
     def move_up(self, pixels):
+        """
+        Move the paddle up by given number of pixels.
+        """
         self.rect.y -= pixels
 
     def move_down(self, pixels):
+        """
+        Move the paddle down by given number of pixels.
+        """
         self.rect.y += pixels
 
-    def check_keys(self, pHeight, screenHeight):
+    def check_keys(self, paddle_height, screen_height):
+        """
+        Check and handle keyboard input for paddle movement.
+        """
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
             if (self.rect.y > 0):
                 self.move_up(self.velocity)
 
         if keys[pygame.K_DOWN]:
-            if (self.rect.y + pHeight) < screenHeight:
+            if (self.rect.y + paddle_height) < screen_height:
                 self.move_down(self.velocity)
